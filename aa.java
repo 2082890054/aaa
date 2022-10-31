671fdb8ddd17d6d72cec5e932d9cbc9e909c517c
cjj <2082890054@qq.com>
2022-10-31 16:24:57 +0800

    second commit

---
+++ aa.java
@@ -0,0 +1,23 @@
+﻿import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @author cjj
+ * @create 2022/10/29-12:20
+ */
+public class aa {
+    public static void main(String[] args) {
+        aa a=new aa();
+        int [] b=new int[]{3,4,3,3};
+        a.singleNumber(b);
+    }
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+        System.out.println("sdadassdasd");
+        }
+        return 0;
+    }
+}
